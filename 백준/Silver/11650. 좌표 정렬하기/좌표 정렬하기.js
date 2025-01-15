79281e6fead2b2313ccac61c75fa9e6aad336938
sksmsalsndi <84209687+minwoo1119@users.noreply.github.com>
2025-01-15 20:14:48 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3952 ms, Memory: 62928 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,20 @@
+const input = require('fs')
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split('\n');
+
+const num = +input[0];
+const data = input.slice(1).map((line) => {
+	const [x, y] = line.split(' ').map(Number);
+	return { x, y };
+});
+
+data.sort((a, b) => {
+	if (a.x === b.x) {
+		return a.y - b.y;
+	}
+	return a.x - b.x;
+});
+
+data.forEach((point) => console.log(point.x, point.y));
